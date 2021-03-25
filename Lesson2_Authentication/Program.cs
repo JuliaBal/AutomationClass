aafa836abf45674819f739bfe374cbe1c6aa15f7
Iuliia Isaeva <iuliia.isaeva@arcadia.spb.ru>
2021-03-25 14:32:54 +0300

    Created a new branch. Changed solutions for exercises from lesson1. Created solution for calculator

---
+++ Lesson2_Authentication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson2_Authentication
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] login_and_password;
+            login_and_password = new string [,] { {"user1", "password1"}, { "user2", "password2" }, { "user3", "password3" }};
+
+        }
+    }
+}
