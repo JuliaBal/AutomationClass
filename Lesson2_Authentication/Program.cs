9850b06279290f26aefa10421f038c75a7f2a29b
Iuliia Isaeva <iuliia.isaeva@arcadia.spb.ru>
2021-03-26 17:10:03 +0300

    complete exercise with authentication

--- Lesson2_Authentication/Program.cs
+++ Lesson2_Authentication/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lesson2_Authentication
 {
-    
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            string[,] login_and_password;
-            login_and_password = new string [,] { {"user1", "password1"}, { "user2", "password2" }, { "user3", "password3" }};
+            Dictionary<string, string> users = new Dictionary<string, string>
+            {
+                { "user1", "password1" },
+                { "user2", "password2" },
+                { "user3", "password3" }
+            };
+            Console.WriteLine("Please enter your login");
+            string login = Console.ReadLine();
 
+            if (users.ContainsKey(login))
+            {
+                string password = users.GetValueOrDefault(login);
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Please enter your password");
+                    string value = Console.ReadLine();
+                    if (value == password)
+                    {
+                        Console.WriteLine("Welcome");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect password. Please enter your password!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect login");
+            }
         }
     }
 }
