1fcee9fb2f9a0a8d84c8aed0758e2e2b27714f25
Iuliia Isaeva <iuliia.isaeva@arcadia.spb.ru>
2021-05-18 19:33:07 +0300

    Created a new branch, which based on master branch.  Completed the first task.

---
+++ Lesson3_Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson3_Ex2
+{
+    abstract class Figure
+    {
+        public abstract int AreaCalculator();
+        public abstract int PerimeterCalculator();
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
