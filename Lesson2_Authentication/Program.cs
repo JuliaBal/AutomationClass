b90b62779e835c2bf7b9203793902d56062eef83
Iuliia Isaeva <iuliia.isaeva@arcadia.spb.ru>
2021-06-02 17:15:23 +0300

    Draft branch

---
+++ Lesson2_Authentication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson2_Authentication
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, string> users = new Dictionary<string, string>
+            {
+                { "user1", "password1" },
+                { "user2", "password2" },
+                { "user3", "password3" }
+            };
+            Console.WriteLine("Please enter your login");
+            string login = Console.ReadLine();
+
+            if (users.ContainsKey(login))
+            {
+                string password = users.GetValueOrDefault(login);
+                for (int i = 0; i < 3; i++)
+                {
+                    int attempt = 2 - i;
+                    Console.WriteLine("Please enter your password");
+                    string value = Console.ReadLine();
+                    if (value == password)
+                    {
+                        Console.WriteLine("Welcome");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect password. Please enter your password!");
+                        Console.WriteLine("you have " + attempt + " attempts left");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect login");
+            }
+        }
+    }
+}
---
+++ Lesson3_Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Convert = ;
+
+namespace Lesson3_Matrix
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter size of matrix: ");
+            string strsize = Console.ReadLine();
+            int size = Convert.ConvertToIntegerFromString(strsize);
+            if (size < 1)
+            {
+                Console.WriteLine("Please enter integer number >= 1");
+            }
+            int[,] array = new int[size, size];
+            Random random = new Random();
+            int sum = 0;
+            int i, j;
+            Console.WriteLine("Your matrix: ");
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    array[i, j] = random.Next(0, 9);
+                    Console.Write(array[i, j] + " ");
+                    if (i == j)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Matrix trace is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
